60d6a7fa3824f497ae543b62e09fbe956d6d5693
Charitha Basnayake <crwbasnayake@gmail.com>
2022-12-04 10:47:17 +0530

    Adding deployment config

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,6 +12,7 @@ import 'package:flutter/material.dart';
 // import 'dart:io' if (dart.library.html) 'dart:ui' as ui;
 import 'dart:ui' as ui;
 import 'dart:html' as html;
+import 'package:pie_chart/pie_chart.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -24,12 +25,82 @@ class HomeScreen extends StatelessWidget {
 
     final theme = Theme.of(context).colorScheme.onSurface;
 
+    final dataMap = <String, double>{
+      "Flutter": 5,
+      "React": 3,
+      "Xamarin": 2,
+      "Ionic": 2,
+    };
+
+    final legendLabels = <String, String>{
+      "Flutter": "Flutter legend",
+      "React": "React legend",
+      "Xamarin": "Xamarin legend",
+      "Ionic": "Ionic legend",
+    };
+
+    final colorList = <Color>[
+      const Color(0xfffdcb6e),
+      const Color(0xff0984e3),
+      const Color(0xfffd79a8),
+      const Color(0xffe17055),
+      const Color(0xff6c5ce7),
+    ];
+
+    final gradientList = <List<Color>>[
+      [
+        const Color.fromRGBO(223, 250, 92, 1),
+        const Color.fromRGBO(129, 250, 112, 1),
+      ],
+      [
+        const Color.fromRGBO(129, 182, 205, 1),
+        const Color.fromRGBO(91, 253, 199, 1),
+      ],
+      [
+        const Color.fromRGBO(175, 63, 62, 1.0),
+        const Color.fromRGBO(254, 154, 92, 1),
+      ]
+    ];
     return Expanded(
         child: Column(children: <Widget>[
-      Container(
-        height: 300,
-        color: Color.fromARGB(255, 232, 232, 232),
-        child: Iframe(),
+      Row(
+        children: [
+          Container(
+            height: 300,
+            color: const Color.fromARGB(255, 232, 232, 232),
+            child: Iframe(),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(100.0, 0.0, 0.0, 0.0),
+            child: PieChart(
+              dataMap: dataMap,
+              animationDuration: const Duration(milliseconds: 800),
+              chartLegendSpacing: 32,
+              chartRadius: 100,
+              colorList: colorList,
+              initialAngleInDegree: 0,
+              chartType: ChartType.disc,
+              ringStrokeWidth: 32,
+              centerText: '',
+              legendOptions: const LegendOptions(
+                showLegendsInRow: false,
+                legendPosition: LegendPosition.right,
+                showLegends: true,
+                legendShape: BoxShape.circle,
+                legendTextStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              chartValuesOptions: const ChartValuesOptions(
+                showChartValueBackground: false,
+                showChartValues: true,
+                showChartValuesInPercentage: false,
+                showChartValuesOutside: true,
+                decimalPlaces: 2,
+              ),
+            ),
+          ),
+        ],
       ),
       const WatchList()
     ]));
